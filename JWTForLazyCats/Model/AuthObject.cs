51c3b23e16cbb3485a1e858149db41dca2dc7da5
Michal Boczula <michal.boczula0101@gmail.com>
2022-04-26 22:44:50 +0200

    Add project files.

---
+++ JWTForLazyCats/Model/AuthObject.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTForLazyCats.Model
+{
+    public class AuthObject
+    {
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
---
+++ JWTForLazyCats/Service/UserService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+
+namespace JWTForLazyCats.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetUser()
+        {
+            var result = string.Empty;
+            if (_httpContextAccessor.HttpContext != null)
+            {
+                result = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+            }
+            return result;
+        }
+    }
+}
